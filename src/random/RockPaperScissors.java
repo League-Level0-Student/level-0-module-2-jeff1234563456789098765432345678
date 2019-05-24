9082eec5b156d51aa1332e00a0ec406e121fe83d
League Student <league@iMac.attlocal.net>
2019-05-23 18:27:32 -0700

    IntroToRandom,RockPaperScissors,DiceRoll(IP)
    remember to fix Diceroll thing

--- src/random/RockPaperScissors.java
+++ src/random/RockPaperScissors.java
@@ -6,6 +6,7 @@ package random;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -73,16 +74,20 @@ public class RockPaperScissors extends JPanel implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
+    	Random rand=
+    			new Random();
+    		int r = rand.nextInt(3);
+    		
         
-        //1. Run the program and play many rounds of Rock Paper Scissors. Does the computer always choose the same thing?
-
+        //1. Run the program and play many rounds of Rock Paper Scissors. Does the computer always choose the same thing? 
+    	//no
         //2. Change the value of opponentSelection to be a random number between 0 and 2;
 	//   Don't forget to create a Random object.
-	    
+    	
         //2. Change the value of opponentSelection to be a random number between 0 and 2; 
-        int opponentSelection = 0;
+        int opponentSelection = r;
         
-        //3. Run the program again. Is the result different?
+        //3. Run the program again. Is the result different? yes
  
         int selection = 0;
        
