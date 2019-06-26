a959c71822c65e1d52c368f6cf1e2be14ad6c788
League Student <league@iMac.attlocal.net>
2019-06-26 16:26:37 -0700

    Dotrace,Rollercoaster :smile:

---
+++ src/random/Rollercoaster.java
@@ -0,0 +1,10 @@
+package random;
+
+import javax.swing.JOptionPane;
+
+public class Rollercoaster {
+public static void main(String[] args) {
+	String r=JOptionPane.showInputDialog("What hight are you?");
+	if (r == "48"); 
+		JOptionPane.showMessageDialog(null, "You may ride the RollerCoaster");}
+} if else (r == "48");
