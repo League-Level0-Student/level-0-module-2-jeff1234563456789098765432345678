998457a9a4f610693351801b7eca96706788f811
League Student <league@iMac-11.attlocal.net>
2019-07-17 16:23:39 -0700

    Icecream,Rollercoaster
    read the learn section

--- src/random/Rollercoaster.java
+++ src/random/Rollercoaster.java
@@ -6,5 +6,10 @@ public class Rollercoaster {
 public static void main(String[] args) {
 	String r=JOptionPane.showInputDialog("What hight are you?");
 	if (r == "48"); 
-		JOptionPane.showMessageDialog(null, "You may ride the RollerCoaster");}
-} if else (r == "48");
+		JOptionPane.showMessageDialog(null, "You may ride the RollerCoaster");
+		
+ else JOptionPane.showMessageDialog(null, ("Grow taller!"));
+		
+
+		
+}}
